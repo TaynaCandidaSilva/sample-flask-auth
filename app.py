e775c754e9fe89cc62eff078048d5726597734c0
TaynaCandidaSilva~ <tayna2507@gmail.com>
2024-10-19 17:32:48 -0300

    criando rota para logout do usuario

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user
+from flask_login import (
+    LoginManager,
+    login_user,
+    current_user,
+    logout_user,
+    login_required,
+)
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
@@ -14,15 +21,15 @@ login_manager.init_app(app)
 
 login_manager.login_view = "login"
 
-login_manager.user_loader
 
-
+@login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
 
 
 @app.route("/login", methods=["POST"])
 def login():
+
     data = request.json
     username = data.get("username")
     password = data.get("password")
@@ -36,6 +43,13 @@ def login():
             return jsonify({"message": "Autenticação realizada com sucesso"})
 
     return jsonify({"message": "Credenciais inválidas"}), 400
+
+
+@app.route("/logout", methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso!"})
 
 
 @app.route("/hello-world", methods=["GET"])
